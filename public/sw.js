912c0434600b1bcd3033be4ac65563b25141ee3d
unknown <xyz>
2024-02-10 02:10:11 -0500

    first comment

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-62166200ebdd7f10b01f.js"
+    "url": "webpack-runtime-9e4cf08e075b1d03b1d0.js"
   },
   {
     "url": "styles.0962d34e5341ace56f50.css"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "fa6a7fbc1be760a4c379525a7eef5bdb"
+    "revision": "6c929843bdaf8362333200bab7d34fd0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-c34fb9d463147b2aee24.js"
@@ -51,7 +51,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "e504fba20e337a27fd703be6acb17b03"
+    "revision": "7e0f43cd5536eff1e557fb8288219131"
   },
   {
     "url": "manifest.webmanifest",
